97c51f087bbd70689b896937bb995e11dc6d0822
Werner Freiberger <wfreiberger@nemetschek.com>
2017-11-06 17:33:45 +0100

    added group controller

---
+++ app/Http/Controllers/GroupController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wfreiberger
+ * Date: 06.11.17
+ * Time: 17:31
+ */
+
+namespace App\Http\Controllers;
+
+use App\Organization;
+use App\User_;
+use App\Groups_orgs;
+
+class GroupController extends Controller {
+
+
+}
